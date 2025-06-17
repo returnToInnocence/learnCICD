00fd4325d27d92dfe86c7e2aa1992cde9e6de21e
Hongjun Li <returntoinnocencegeek@outlook.com>
2025-06-17 09:25:59 +0800

    ci: debug ci

---
+++ index.js
@@ -0,0 +1,10 @@
+// 简单的Node.js应用程序
+console.log("Hello, World!");
+console.log("这是一个学习CI/CD的项目");
+
+// 导出一个简单的函数用于测试
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+module.exports = { greet }; 
